c0d6cb1c5f7f4776e79a5d3be03b803f71cb86a3
cxxxr <g23tlm@gmail.com>
2018-12-09 20:06:38 +0900

    example/goto.cを動くように変更

--- example/goto.c
+++ example/goto.c
@@ -1,8 +1,12 @@
-loop(n) {
-  start:
-  f(n);
-  n = n + 1;
-  if (n < 10) {
-    goto start;
-  }
+loop(n, end) {
+        start:
+        log(n);
+        n = n + 1;
+        if (n <= end) {
+                goto start;
+        }
+}
+
+main() {
+        loop(1, 5);
 }
